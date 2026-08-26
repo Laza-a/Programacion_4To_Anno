ce98b41a383b775c546612990b7201999ed86580
Lázaro_Medeot <Lmedeotsimioni@escuelasproa.edu.ar>
2026-08-26 10:11:14 -0300

    Def clases del TP4

---
+++ POO-OOP/P4/__pycache__/Main.py
@@ -0,0 +1,8 @@
+"""
+Práctico 2  Sistema Bancario
+Desarrollar un pequeño sistema bancario utilizando Programación Orientada a Objetos.
+Crear una clase padre CuentaBancaria y dos clases hijas: CajaAhorro y CuentaCorriente.
+El saldo deberá estar encapsulado. Las cuentas deberán permitir consultar saldo, depositar y extraer dinero. La CajaAhorro deberá permitir aplicar intereses y la CuentaCorriente realizar extracciones con comisión.
+Crear objetos de ambas clases y realizar diferentes operaciones para comprobar su funcionamiento.
+Conceptos a utilizar: clases, objetos, __init__(), métodos, encapsulamiento, herencia y super().
+"""
